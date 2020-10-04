6a90ec27187b100794e94106fceaaca3c1dba068
Jorge Avalos <jorgeavalosaaa@gmail.com>
2020-10-04 18:18:15 -0500

    Esta es la base del proyecto. Estan todas las vistas.

---
+++ README.md
@@ -0,0 +1,25 @@
+Este proyecto está basado en la página https://demo.htmlhunters.com/shopy/index.html
+
+MERN + MySQL + Git + Deployment + HTML/CSS
+
+JavaScript para el DOM
+
+MySQL
+
+Git
+
+HTML/CSS
+
+Deployment
+
+CRUD APP con Mysql
+
+Mongo
+
+CRUD APP con Mongo
+
+React
+
+CRUD APP con Mysql y React
+
+CRUD APP con Mongo y React
---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Lo que me imagino es crear funciones para cada funcionalidad que hay en la página, para no repetir nada.
+
+// Me imagino un main en el que solo recolecto los objetos, aunque supongo que eso es para cada layout.
+
+// Creo que puedo tener un main que llame al resto, o quizás no.
+
+// Debería de usar MVC en el front-end? ¿Hay alguna arquitectura?
+
+// Qué pasa si quiero consumir alguna API?
+
+// Hay alguna forma en la que pueda usar objetos? No lo sé.
+
+// Si llego a utilizar una API supongo que sería para las compras, aunque quizá eso se hace en el back-end. ¿Cómo se llama una API en el back-end? Creo que ya lo he hecho.
